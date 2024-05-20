149620ef8e4755b93250b4339dfbfd59d0644169
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-05-20 22:02:57 +0530

    lifting state up

--- far_away/src/App.jsx
+++ far_away/src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Components/Logo";
 import Form from "./Components/Form";
 import PackingList from "./Components/PackingList";
 import States from "./Components/States";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((crrItems) => [...crrItems, item]);
+  };
+
   return (
     <div>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <States />
     </div>
   );
--- far_away/src/Components/PackingList.jsx
+++ far_away/src/Components/PackingList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { initialItems } from "../assets/data.js";
+//import { initialItems } from "../assets/data.js";
 import Item from "./Item";
 
-const PackingList = () => {
+const PackingList = ({ items }) => {
   return (
     <div className="bg-yellow-900 min-h-[76vh] p-5">
       <h1 className="uppercase underline font-medium text-lg text-center text-white">
         Lists
       </h1>
       <ul className="grid grid-cols-4 gap-4 m-10 justify-items-center">
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
